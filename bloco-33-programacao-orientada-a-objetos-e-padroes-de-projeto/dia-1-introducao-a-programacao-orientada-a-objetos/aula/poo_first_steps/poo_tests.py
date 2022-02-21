638841876afc07bd6c41503aa640007c4cd0c57d
Luiz Henrique <luishenriqueee2010@hotmail.com>
2022-02-21 20:16:56 -0300

    add: finish exercises

--- bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/aula/poo_first_steps/poo_tests.py
+++ bloco-33-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/aula/poo_first_steps/poo_tests.py
@@ -1,8 +1,5 @@
 A = []
 
-# levar pra mentoria
-# Self é adicionado ao array A antes de ter os valores atribuidos, mas ainda assim, nos consoles da linha 17 e 20, ele retorna o nome.
-
 
 class User:
     def __init__(self, name, email, password):
@@ -20,6 +17,9 @@ new_user = User("José Mourinho", "specialone@rome.com", "5pec1al0ne")
 
 for elem in A:
     print(elem.name)
-    print(new_user)
+    print(new_user.name)
     print(elem == new_user)
-    print(elem == User)
+
+
+# levar pra mentoria
+# Self é adicionado ao array A antes de ter os valores atribuidos, mas ainda assim, nos consoles da linha 17 e 20, ele retorna o nome.
